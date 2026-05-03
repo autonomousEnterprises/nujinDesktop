e326c4a3b899840e2c3a0bd57d74484569b01e92
christonomous <chris@brane.media>
2026-05-03 17:27:34 +0200

    refactor: update dashboard grid to use container queries and optimize responsiveness

--- src/renderer/src/components/Dashboards/DashboardGrid.tsx
+++ src/renderer/src/components/Dashboards/DashboardGrid.tsx
@@ -12,13 +12,13 @@ const getGridClasses = (gridSize: string = "medium") => {
     case "small":
       return "col-span-1 row-span-1";
     case "medium":
-      return "col-span-1 md:col-span-1 lg:col-span-1 row-span-1";
+      return "col-span-1 row-span-1";
     case "wide":
-      return "col-span-1 md:col-span-2 lg:col-span-2 row-span-1";
+      return "col-span-1 @min-[800px]:col-span-2 row-span-1";
     case "tall":
       return "col-span-1 row-span-2";
     case "large":
-      return "col-span-1 md:col-span-2 lg:col-span-2 row-span-2";
+      return "col-span-1 @min-[800px]:col-span-2 row-span-2";
     case "full":
       return "col-span-full";
     default:
@@ -28,11 +28,11 @@ const getGridClasses = (gridSize: string = "medium") => {
 
 export default function DashboardGrid({ dashboard, profile }: DashboardGridProps) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 auto-rows-[240px] gap-8 p-1 pb-10">
+    <div className="@container/dashboard-grid grid grid-cols-1 @min-[600px]:grid-cols-2 @min-[1100px]:grid-cols-3 @min-[1400px]:grid-cols-4 auto-rows-[240px] gap-6 md:gap-8 p-1 pb-10">
       {dashboard.widgets.map((widget) => (
         <div 
           key={widget.id} 
-          className={`${getGridClasses(widget.gridSize)} transition-all duration-700 ease-in-out`}
+          className={`${getGridClasses(widget.gridSize)} transition-all duration-500 ease-in-out`}
         >
           <WidgetRenderer 
             widget={widget} 
